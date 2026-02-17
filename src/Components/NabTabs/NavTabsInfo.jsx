4ff2ac59608fd42c70737db23e927af4f8d346fc
Camilo <camilopb0405@gmail.com>
2026-02-16 19:29:04 -0500

    nada nuevo

---
+++ src/Components/NabTabs/NavTabsInfo.jsx
@@ -0,0 +1,89 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Tabs, { tabsClasses } from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+
+import Experience from "./Experience";
+import Projects from "./Projects";
+import Contact from "./Contact";
+import "../../App.css";
+
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+export default function Navtab() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box
+        sx={{
+          borderBottom: 1,
+          borderColor: "divider",
+          placeSelf: "center",
+          textColor: "white",
+        }}
+      >
+        <Tabs
+          sx={{
+            color: "white",
+            "&:hover": { color: "#3b82f6" },
+            fontSize: 20,
+          }}
+          value={value}
+          onChange={handleChange}
+          textColor="primary"
+          indicatorColor="primary"
+          aria-label="secondary tabs example"
+          variant="scrollable"
+          visibleScrollbar="true"
+          scrollButtons="true"
+        >
+          <Tab className="text-white" label="Projects" {...a11yProps(0)} />
+          <Tab label="Experience" {...a11yProps(1)} />
+          <Tab label="Contact" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        <Projects />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <Experience />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        <Contact />
+      </CustomTabPanel>
+    </Box>
+  );
+}
