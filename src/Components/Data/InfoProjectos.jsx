3c971f75241f1e78db9842912c60e5c98394c1e7
Camilo <camilopb0405@gmail.com>
2026-02-19 14:05:20 -0500

    nuevo projecto añadido

--- src/Components/Data/InfoProjectos.jsx
+++ src/Components/Data/InfoProjectos.jsx
@@ -14,27 +14,32 @@ import git from "@/assets/git.svg";
 import tailwind from "@/assets/tailwind.svg";
 import ViteLogo from "@/assets/vite.svg";
 import InvestmentCalculator from "@/assets/InvestmentCalculator.png";
+import CountDownGame from "@/assets/CountDownGame.png";
+import { Button } from "@mui/material";
+
 const projects = [
   {
     key: 1,
     id: 1,
     title: "Online Catalog Perfumes T11",
-    description: "Online perfume catalog for a barbershop in Barranquilla ",
+    description:
+      "My first Demo App, and is a Web Catalog for a Local perfume Shoo Located in Barranquilla",
     tech: ["html", "CSS", "git"],
     techIcons: [html, css, git],
     image: t11,
-    github: "https://github.com/usuario/portfolio",
+    github: "https://github.com/itscamperezb/T11PERFUMES.git",
     demo: "https://itscamperezb.github.io/T11PERFUMES/",
   },
   {
     key: 2,
     id: 2,
     title: "SIMON DICE GAME",
-    description: "Online version of Simon Says Game",
+    description:
+      "Online version of Simon Says Game created to apply the manipulation of arrays, using push methods, validation and conditional Styling",
     tech: ["js", "nodejs", "git"],
     techIcons: [js, node, git],
     image: simonDice,
-    github: "https://github.com/usuario/portfolio",
+    github: "https://github.com/itscamperezb/Simon-Dice-Game.git",
     demo: "https://itscamperezb.github.io/Simon-Dice-Game/",
   },
 
@@ -42,11 +47,11 @@ const projects = [
     key: 3,
     id: 3,
     title: "Jobify App",
-    description: "Full stack Mobile App development with Jobify Team",
+    description: "Full stack Mobile App in development with Jobify Team",
     tech: ["React", "js", "NextJs", "Css", "Tailwind", "git"],
     techIcons: [reactLogo, js, nextjs, css, tailwind, git],
     image: Jobify,
-    github: "https://github.com/usuario/portfolio",
+    github: "https://github.com/jobifyhq",
     demo: "https://github.com/jobifyhq/jobify-webapp",
   },
   {
@@ -57,31 +62,44 @@ const projects = [
     tech: ["React", "CSS", "Vite", "html", "git"],
     techIcons: [reactLogo, css, git, ViteLogo],
     image: KeeperApp,
-    github: "https://itscamperezb.github.io/MyKeeperApp/",
+    github: "https://github.com/itscamperezb/MyKeeperApp.git",
     demo: "https://itscamperezb.github.io/MyKeeperApp/",
   },
   {
     key: 5,
     id: 5,
     title: "Tic-Tac-Toe Game",
-    description: "A basic Tic-Tac-Toe Game",
+    description:
+      "Is a tic-tac-toe demo App created to implement programing logic using objects, arrays and useSate Hooks",
     tech: ["React", "Vite", "CSS", "html", "git"],
     techIcons: [reactLogo, css, git, ViteLogo],
     image: TicTacToe,
-    github: "https://itscamperezb.github.io/TIC-TAC-TOE/",
+    github: "https://github.com/itscamperezb/TIC-TAC-TOE.git",
     demo: "https://itscamperezb.github.io/TIC-TAC-TOE/",
   },
   {
     key: 6,
     id: 6,
-    title: "Investment Calculatro",
+    title: "Investment Calculator",
     description:
-      "An investment Calculator so the people can calculate how much money can tehy get for investing with certain interest",
+      "An investment Calculator so the people can calculate how much money can they get for investing with certain interest",
     tech: ["React", "Vite", "CSS", "html", "git"],
     techIcons: [reactLogo, css, git, ViteLogo],
     image: InvestmentCalculator,
-    github: "https://itscamperezb.github.io/TIC-TAC-TOE/",
-    demo: "https://itscamperezb.github.io/TIC-TAC-TOE/",
+    github: "https://github.com/itscamperezb/Invesment-Calculator.git",
+    demo: "https://itscamperezb.github.io/Invesment-Calculator/",
+  },
+  {
+    key: 7,
+    id: 7,
+    title: "CountDown-Game",
+    description:
+      "This is a CountDown-Game where I implement the use of Hooks like useRef and useState, using programing logic to render and calculate the Score",
+    tech: ["React", "Vite", "CSS", "html", "git"],
+    techIcons: [reactLogo, css, git, ViteLogo],
+    image: CountDownGame,
+    github: "https://github.com/itscamperezb/CountdownGame.git",
+    demo: "https://itscamperezb.github.io/CountdownGame/",
   },
 ];
 
@@ -92,7 +110,7 @@ function Projectos() {
         <div key={project.key}>
           <a href={project.demo}>
             <div
-              className="bg-(--card-color) m-2 p-2 rounded-lg hover:scale-105 transition-transform duration-300 cursor-pointer h-full"
+              className="grid grid-cols-1 bg-(--card-color) m-2 p-2 rounded-lg hover:scale-105 transition-transform duration-300 cursor-pointer h-full"
               key={project.id}
             >
               <div className="justify-items-center">
@@ -110,6 +128,7 @@ function Projectos() {
                 <p className="w-[70%] text-white text-md">
                   {project.description}
                 </p>
+
                 <div className="grid grid-cols-2 gap-4">
                   {project.techIcons.map((icon, index) => (
                     <img
@@ -120,6 +139,11 @@ function Projectos() {
                     />
                   ))}
                 </div>
+              </div>
+              <div className="m-4 place-content-center">
+                <Button variant="contained" href={project.github}>
+                  Repository
+                </Button>
               </div>
             </div>
           </a>
